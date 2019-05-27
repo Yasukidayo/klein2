36d3dd2d652c2e257353392c40afd5c6199ffdd7
Yasukidauyo <hirayasuki@occ.co.jp>
2019-05-27 17:59:34 +0900

    initial commit

---
+++ WebApplication1/WebApplication1/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        public long Id { get; set; }　　//主キー
+        public string Name { get; set; } //カラム
+        public string Password { get; set; }　//カラム
+        public bool IsAdmin { get; set; }　//カラム
+    }
+}
