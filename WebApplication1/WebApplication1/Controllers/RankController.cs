92a3377b001298a8f5a8dbea09c67ac5b47eb273
Yasukidauyo <hirayasuki@occ.co.jp>
2019-06-10 16:26:58 +0900

    add

---
+++ WebApplication1/WebApplication1/Controllers/RankController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class RankController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+        public RankController(ApplicationContext context)
+        {
+            _context = context;
+        }
+    }
+}
